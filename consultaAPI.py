2ab1740ec3197adf1f6303aae8558518baffb1bd
Aluno da Infinity School <aluno@3D.local>
2023-09-29 10:47:14 -0300

    atualização da saída da função pegar_cotacoes

--- consultaAPI.py
+++ consultaAPI.py
@@ -4,6 +4,6 @@ def pegar_cotacoes(codigo_moeda):
         requisicao = requests.get(f"https://economia.awesomeapi.com.br/last/{codigo_moeda}-BRL")
         requisicao_dic = requisicao.json()
         cotacao = requisicao_dic[f'{codigo_moeda}BRL']['bid']
-        return f'A cotação atual da moeda: {cotacao}'
+        return f'A cotação atual do par {codigo_moeda}/BRL: R$ {format(float(cotacao),".2f").replace(".", ",")}'
     except:
         return "Código de Moeda Inválido"
